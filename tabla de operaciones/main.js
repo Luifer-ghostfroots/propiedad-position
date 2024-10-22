0d90f3df9e779ba6a60a7f4f248ae9959e059d0a
Luifer-ghostfroots <luifernandomojica@gmail.com>
2024-10-22 11:21:42 -0500

    agregue el proyecto de las tablas
    Es el trabajo que se usa para crear una tabla de multiplicar funcional

---
+++ tabla de operaciones/main.js
@@ -0,0 +1,36 @@
+function calcular(){
+    let numero= document.getElementById("inputnum").value
+     
+    document.getElementById("multiplicacion1").textContent=`${numero} x 1 = `
+    document.getElementById("resultado1").textContent=`${numero * 1}`
+
+    document.getElementById("multiplicacion2").textContent=`${numero} x 2 = `
+    document.getElementById("resultado2").textContent=`${numero * 2}`
+
+    document.getElementById("multiplicacion3").textContent=`${numero} x 3 = `
+    document.getElementById("resultado3").textContent=`${numero * 3}`
+
+    document.getElementById("multiplicacion4").textContent=`${numero} x 4 = `
+    document.getElementById("resultado4").textContent=`${numero * 4}`
+
+    document.getElementById("multiplicacion5").textContent=`${numero} x 5 = `
+    document.getElementById("resultado5").textContent=`${numero * 5}`
+
+    document.getElementById("multiplicacion6").textContent=`${numero} x 6 = `
+    document.getElementById("resultado6").textContent=`${numero * 6}`
+
+    document.getElementById("multiplicacion7").textContent=`${numero} x 7 = `
+    document.getElementById("resultado7").textContent=`${numero * 7}`
+
+    document.getElementById("multiplicacion8").textContent=`${numero} x 8 = `
+    document.getElementById("resultado8").textContent=`${numero * 8}`
+
+    document.getElementById("multiplicacion9").textContent=`${numero} x 9 = `
+    document.getElementById("resultado9").textContent=`${numero * 9}`
+
+    document.getElementById("multiplicacion10").textContent=`${numero} x 10 = `
+    document.getElementById("resultado10").textContent=`${numero * 10}`
+
+
+
+} 
