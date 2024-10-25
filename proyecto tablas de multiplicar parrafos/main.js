f00879e0ad317c5d696026522697af9e3dfa78f0
Luifer-ghostfroots <luifernandomojica@gmail.com>
2024-10-25 12:00:12 -0500

    una tabla de multiplicar
    al poner un numero aleatorio se multiplica en todos las casillas

---
+++ proyecto tablas de multiplicar parrafos/main.js
@@ -0,0 +1,15 @@
+function calculo(){
+    let numero = document.getElementById("numero").value
+
+    document.getElementById("texto1").textContent = numero + " x 1 = " + (numero*1)
+    document.getElementById("texto2").textContent = numero + " x 2 = " + (numero*2)
+    document.getElementById("texto3").textContent = numero + " x 3 = " + (numero*3)
+    document.getElementById("texto4").textContent = numero + " x 4 = " + (numero*4)
+    document.getElementById("texto5").textContent = numero + " x 5 = " + (numero*5)
+    document.getElementById("texto6").textContent = numero + " x 6 = " + (numero*6)
+    document.getElementById("texto7").textContent = numero + " x 7 = " + (numero*7)
+    document.getElementById("texto8").textContent = numero + " x 8 = " + (numero*8)
+    document.getElementById("texto9").textContent = numero + " x 9 = " + (numero*9)
+    document.getElementById("texto10").textContent = numero + " x 10 = " + (numero*10)
+
+}
